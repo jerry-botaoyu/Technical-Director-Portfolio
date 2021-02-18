86638b3afa0df0403ae30289d3c2398da4b5f759
jerry-botaoyu <35499710+jerry-botaoyu@users.noreply.github.com>
2021-02-18 14:12:30 -0500

    Update multi_objects_attribute_editor.py
    Modifies both transform and mesh objects

--- multi_objects_attribute_editor.py
+++ multi_objects_attribute_editor.py
@@ -3,18 +3,15 @@ import maya.cmds as cmds
 
 BUTTON_HEIGHT = 50
 OUTPUT_HEIGHT = 30
-# IMPORTANT: this script only does the transformNode, and does NOT do the shapeNode. This is why castShadows attributes
-#            cannot be edited using this script.
+# IMPORTANT: this script only works on the following object: transform, and mesh.
+# TODO: specifying what attributes are normally modified will optimize this script. 
+#       this is because each object will have about 200 attributes and not all of these attributes
+#       should be copied onto the child objects.
 
-# NOTE: the naming covention for shape change. For example, when creating 2 identical cubes, it will be cube1, and cube2.
-#       their respective shape node will be cubeShape1 and cubeShape2. However, if you rename the cube as cube3, and cube4.
-#       their respective shape node will be cube3Shape and cube4Shape.
+transformObjAttributes = {}
+meshObjAttributes = {}
 
-# TODO: verify the node names are used correctly
-# TODO: do other node such as shape, poly (cube, plane, etc.)
-
-transformNodeAttributes = {}
-
+#there are attributes that should be locked no matter what
 IGNORED_ATTRIBUTE = ["translateX", "translateY", "translateZ"] 
 
 def initializeEditor():
@@ -25,28 +22,56 @@ def initializeEditor():
     cmds.button( label="Step 3: Make the childs' attributes same as parent", command=followParent, bgc=[0.47, 1, 0.04], height=BUTTON_HEIGHT)
     cmds.showWindow()
 
+# Reinitializes parent objects, and their attributes. 
 def getParent(self):
-    global transformNodeAttributes
-    transformNodeAttributes = {}
-    parentObj = cmds.ls(selection=True)[0]
-    allAttributes = cmds.listAttr(parentObj)
+    transformObjAttributes = {}
+    meshObjAttributes = {}
+    parentObjs = cmds.ls(selection=True, dag=True)
+    callBacksForRightType(populateObj, parentObjs)
+    
+
+# Populates the attributes of parent objects
+def populateObj(objName, objAttributesReference):
+    print "---------------------------------------------- populateObj"
+    errorAttributes = []
+    allAttributes = cmds.listAttr(objName)
     for attribute in allAttributes:
-        fullAttributeName = parentObj + '.' + attribute
+        fullAttributeName = objName + '.' + attribute
         value = None
         try:
             value = cmds.getAttr(fullAttributeName, sl=True)
         except:
-            print "Error for " + fullAttributeName
-        transformNodeAttributes[attribute] = value
-        #print attribute + ": ", value
-                
+            errorAttributes.append(fullAttributeName)
+        objAttributesReference[attribute] = value
+    print "For ", objName, ", values cannot get from these attributes: ", errorAttributes
+
+# Copies parent objects' attributes into child objects' attributes                
 def followParent(self):
-    childObjs = cmds.ls(selection=True)
-    for childObj in childObjs: 
-        for attribute in transformNodeAttributes:
-            fullAttributeName = childObj + '.' + attribute
-            if attribute not in IGNORED_ATTRIBUTE:
-                try:
-                    cmds.setAttr(fullAttributeName, transformNodeAttributes[attribute])
-                except:
-                    print "Error for " + fullAttributeName
+    childObjs = cmds.ls(selection=True, dag=True)
+    callBacksForRightType(setChildAttributes, childObjs)
+
+# modifies the correct attributes' type depending on the objects' type
+def callBacksForRightType(callBackFunction, objs):
+    global transformObjAttributes, meshObjAttributes
+    for obj in objs: 
+        if cmds.objectType(obj) == "transform":
+            callBackFunction(obj, transformObjAttributes)
+        elif cmds.objectType(obj) == "mesh":
+            callBackFunction(obj, meshObjAttributes)
+        else:
+            print "Error: ", cmds.objectType(obj), " is unsupported."
+
+# copies the parents' attributes into the childs' attributes
+def setChildAttributes(objName, objAttributesReference):
+    print "---------------------------------------------- setChildAttributes"
+    errorAttributes = []
+    for attribute in objAttributesReference:
+        fullAttributeName = objName + '.' + attribute
+        if attribute not in IGNORED_ATTRIBUTE:
+            try:
+                cmds.setAttr(fullAttributeName, objAttributesReference[attribute])
+            except:
+                errorAttributes.append(fullAttributeName)
+    print "For ", objName, ", values cannot get from these attributes: ", errorAttributes
+
+
