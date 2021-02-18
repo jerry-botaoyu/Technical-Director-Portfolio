dbddaa676073e16079e220e9bcc1ba3a04ee02a6
jerry-botaoyu <35499710+jerry-botaoyu@users.noreply.github.com>
2021-02-18 17:10:40 -0500

    Update multi_objects_attribute_editor.py
    update UI

--- multi_objects_attribute_editor.py
+++ multi_objects_attribute_editor.py
@@ -2,33 +2,70 @@ import maya.OpenMaya as OpenMaya
 import maya.cmds as cmds
 
 BUTTON_HEIGHT = 50
-OUTPUT_HEIGHT = 30
+OUTPUT_HEIGHT = 50
+ROW_SPACING = 10
 # IMPORTANT: this script only works on the following object: transform, and mesh.
 # TODO: specifying what attributes are normally modified will optimize this script. 
 #       this is because each object will have about 200 attributes and not all of these attributes
 #       should be copied onto the child objects.
 
-transformObjAttributes = {}
-meshObjAttributes = {}
+g_transform_attributes = {}
+g_mesh_attributes = {}
+g_new_transform_attributes = {}
+g_new_mesh_attributes = {}
+diff_transform_attributes = {}
+diff_mesh_attributes = {}
 
 #there are attributes that should be locked no matter what
 IGNORED_ATTRIBUTE = ["translateX", "translateY", "translateZ"] 
 
 def initializeEditor():
     cmds.window( width=350, title="Multiple Object Attribute Editor" )
-    cmds.columnLayout( adjustableColumn=True )
-    output = cmds.text(label="Step 1: Adjust the parent attributes", height=OUTPUT_HEIGHT)
-    cmds.button( label="Step 2: Make the slected object as the parent object", command=getParent, bgc=[1, 1, 0.04], height=BUTTON_HEIGHT)
-    cmds.button( label="Step 3: Make the childs' attributes same as parent", command=followParent, bgc=[0.47, 1, 0.04], height=BUTTON_HEIGHT)
+    cmds.columnLayout( rs=ROW_SPACING, cw=20, columnAttach=('both', 20), adjustableColumn=True )
+    cmds.text("padding", label=" ")
+    cmds.text("output", label="Select the parent object and click the 'record INITIAL attributes' button", height=OUTPUT_HEIGHT, wordWrap=True)
+    cmds.button( label="record INITIAL attributes", command=getInitialAttributes, height=BUTTON_HEIGHT)
+    cmds.button( label="save MODIFIED attributes", command=getModifiedAttributes,  height=BUTTON_HEIGHT)
+    cmds.button( label="apply to children", command=followParent, height=BUTTON_HEIGHT)
     cmds.showWindow()
 
+initializeEditor()
+
 # Reinitializes parent objects, and their attributes. 
-def getParent(self):
-    transformObjAttributes = {}
-    meshObjAttributes = {}
+def getInitialAttributes(self):
+    cmds.text("output", edit=True, label="INITIAL Attributes has been recorded! You may now modify the parent's attributes. \n" +
+              "After the modification, Cllick on the 'save MODIFIED attributes'")
+    print "---------------------------------------------- getParent"
+    global g_transform_attributes, g_mesh_attributes
+    g_transform_attributes = {}
+    g_mesh_attributes = {}
+    g_new_transform_attributes = {}
+    g_new_mesh_attributes = {}
     parentObjs = cmds.ls(selection=True, dag=True)
-    callBacksForRightType(populateObj, parentObjs)
-    
+    # get the initial attributes
+    callBacksForRightType(populateObj, parentObjs, g_transform_attributes, g_mesh_attributes)
+
+def getModifiedAttributes(self):
+    cmds.text("output", edit=True, label="MODIFIED Attributes has been recorded! You may now select the children objects. \n" +
+              "After selecting them, Cllick on the 'apply to children'")
+    global g_transform_attributes, g_mesh_attributes, g_new_transform_attributes, g_new_mesh_attributes
+    global diff_transform_attributes, diff_mesh_attributes
+    parentObjs = cmds.ls(selection=True, dag=True)
+    # get the new attributes, after parent's attributes have changed
+    callBacksForRightType(populateObj, parentObjs, g_new_transform_attributes, g_new_mesh_attributes)
+    diff_transform_attributes = get_differences_between(g_new_transform_attributes, g_transform_attributes)
+    diff_mesh_attributes = get_differences_between(g_new_mesh_attributes, g_mesh_attributes)
+
+# Copies parent objects' attributes into child objects' attributes                
+def followParent(self):
+    cmds.text("output", edit=True, label="Select the parent object and click the 'record INITIAL attributes' button")
+    global diff_transform_attributes, diff_mesh_attributes
+    childObjs = cmds.ls(selection=True, dag=True)
+    callBacksForRightType(setChildAttributes, childObjs, diff_transform_attributes, diff_mesh_attributes)
+
+# find the difference of x and y dictionaries. if there is a difference, keep value of x.
+def get_differences_between(x, y):
+    return {k: x[k] for k in x if k in y and x[k] != y[k]}
 
 # Populates the attributes of parent objects
 def populateObj(objName, objAttributesReference):
@@ -45,19 +82,13 @@ def populateObj(objName, objAttributesReference):
         objAttributesReference[attribute] = value
     print "For ", objName, ", values cannot get from these attributes: ", errorAttributes
 
-# Copies parent objects' attributes into child objects' attributes                
-def followParent(self):
-    childObjs = cmds.ls(selection=True, dag=True)
-    callBacksForRightType(setChildAttributes, childObjs)
-
 # modifies the correct attributes' type depending on the objects' type
-def callBacksForRightType(callBackFunction, objs):
-    global transformObjAttributes, meshObjAttributes
+def callBacksForRightType(callBackFunction, objs, transformAttributes, meshAttributes):
     for obj in objs: 
         if cmds.objectType(obj) == "transform":
-            callBackFunction(obj, transformObjAttributes)
+            callBackFunction(obj, transformAttributes)
         elif cmds.objectType(obj) == "mesh":
-            callBackFunction(obj, meshObjAttributes)
+            callBackFunction(obj, meshAttributes)
         else:
             print "Error: ", cmds.objectType(obj), " is unsupported."
 
@@ -69,6 +100,7 @@ def setChildAttributes(objName, objAttributesReference):
         fullAttributeName = objName + '.' + attribute
         if attribute not in IGNORED_ATTRIBUTE:
             try:
+                print 'attempting to set ', fullAttributeName, ' to ', objAttributesReference[attribute] 
                 cmds.setAttr(fullAttributeName, objAttributesReference[attribute])
             except:
                 errorAttributes.append(fullAttributeName)
