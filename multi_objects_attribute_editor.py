69730ef3b65c003f27e9927188110e757648b4ae
jerry-botaoyu <35499710+jerry-botaoyu@users.noreply.github.com>
2021-02-19 19:36:57 -0500

    Update multi_objects_attribute_editor.py
    refactors names and added more comments

--- multi_objects_attribute_editor.py
+++ multi_objects_attribute_editor.py
@@ -1,109 +1,129 @@
-import maya.OpenMaya as OpenMaya
 import maya.cmds as cmds
+
+"""
+Allows the user to change mesh and transform attributes of an object (this will be known as the parent object),
+save the changes that the user made,
+then apply the changes on multiple other objects (this will be known as the child objects).
+"""
 
 BUTTON_HEIGHT = 50
 OUTPUT_HEIGHT = 50
 ROW_SPACING = 10
-# IMPORTANT: this script only works on the following object: transform, and mesh.
-# TODO: specifying what attributes are normally modified will optimize this script. 
-#       this is because each object will have about 200 attributes and not all of these attributes
-#       should be copied onto the child objects.
 
+"""parent object attributes (initial attributes) before the modification"""
 g_transform_attributes = {}
 g_mesh_attributes = {}
+"""parent object attributes (modified attributes) after the modification"""
 g_new_transform_attributes = {}
 g_new_mesh_attributes = {}
-diff_transform_attributes = {}
-diff_mesh_attributes = {}
+"""
+the difference between the initial and modified attributes,
+the child objects will use the value in these attributes.
+"""
+g_diff_transform_attributes = {}
+g_diff_mesh_attributes = {}
 
-#there are attributes that should be locked no matter what
+"""
+there are attributes that should not be modified 
+"""
 IGNORED_ATTRIBUTE = ["translateX", "translateY", "translateZ"] 
 
-def initializeEditor():
-    cmds.window( width=350, title="Multiple Object Attribute Editor" )
-    cmds.columnLayout( rs=ROW_SPACING, cw=20, columnAttach=('both', 20), adjustableColumn=True )
-    cmds.text("padding", label=" ")
-    cmds.text("output", label="Select the parent object and click the 'record INITIAL attributes' button", height=OUTPUT_HEIGHT, wordWrap=True)
-    cmds.button( label="record INITIAL attributes", command=getInitialAttributes, height=BUTTON_HEIGHT)
-    cmds.button( label="save MODIFIED attributes", command=getModifiedAttributes,  height=BUTTON_HEIGHT)
-    cmds.button( label="apply to children", command=followParent, height=BUTTON_HEIGHT)
+"""
+Initializes the UI elements in the editor
+"""
+def initialize_editor():
+    cmds.window( w=350, t="Multiple Object Attribute Editor" )
+    cmds.columnLayout( rs=ROW_SPACING, cw=20, cat=('both', 20), adj=True )
+    cmds.text("padding", l=" ")
+    cmds.text("output", l="Select the parent object and click the 'record INITIAL attributes' button", h=OUTPUT_HEIGHT, ww=True)
+    cmds.button( l="record INITIAL attributes", c=get_parent_initial_attributes, h=BUTTON_HEIGHT)
+    cmds.button( l="save MODIFIED attributes", c=get_parent_modified_attributes,  h=BUTTON_HEIGHT)
+    cmds.button( l="apply changes to children", c=change_child_objects_attributes, h=BUTTON_HEIGHT)
     cmds.showWindow()
 
-initializeEditor()
-
-# Reinitializes parent objects, and their attributes. 
-def getInitialAttributes(self):
-    cmds.text("output", edit=True, label="INITIAL Attributes has been recorded! You may now modify the parent's attributes. \n" +
+"""
+get parent initial attributes. 
+"""
+def get_parent_initial_attributes(self):
+    cmds.text("output", e=True, l="INITIAL Attributes has been recorded! You may now modify the parent's attributes. \n" +
               "After the modification, Cllick on the 'save MODIFIED attributes'")
-    print "---------------------------------------------- getParent"
     global g_transform_attributes, g_mesh_attributes
     g_transform_attributes = {}
     g_mesh_attributes = {}
     g_new_transform_attributes = {}
     g_new_mesh_attributes = {}
-    parentObjs = cmds.ls(selection=True, dag=True)
-    # get the initial attributes
-    callBacksForRightType(populateObj, parentObjs, g_transform_attributes, g_mesh_attributes)
+    parent_objects = cmds.ls(sl=True, dag=True)
+    call_back_for_right_type(get_attributes, parent_objects, g_transform_attributes, g_mesh_attributes)
 
-def getModifiedAttributes(self):
-    cmds.text("output", edit=True, label="MODIFIED Attributes has been recorded! You may now select the children objects. \n" +
-              "After selecting them, Cllick on the 'apply to children'")
+"""
+get parent modified attributes and
+get the differences between the parent initial attributes and modified attributes.
+"""
+def get_parent_modified_attributes(self):
+    cmds.text("output", e=True, l="MODIFIED Attributes has been recorded! You may now select the children objects. \n" +
+              "After selecting them, Cllick on the 'apply changes to children'")
     global g_transform_attributes, g_mesh_attributes, g_new_transform_attributes, g_new_mesh_attributes
-    global diff_transform_attributes, diff_mesh_attributes
-    parentObjs = cmds.ls(selection=True, dag=True)
-    # get the new attributes, after parent's attributes have changed
-    callBacksForRightType(populateObj, parentObjs, g_new_transform_attributes, g_new_mesh_attributes)
-    diff_transform_attributes = get_differences_between(g_new_transform_attributes, g_transform_attributes)
-    diff_mesh_attributes = get_differences_between(g_new_mesh_attributes, g_mesh_attributes)
+    global diff_transform_attributes, g_diff_mesh_attributes
+    parent_objects = cmds.ls(sl=True, dag=True)
+    call_back_for_right_type(get_attributes, parent_objects, g_new_transform_attributes, g_new_mesh_attributes)
+    g_diff_transform_attributes = get_differences_between(g_new_transform_attributes, g_transform_attributes)
+    g_diff_mesh_attributes = get_differences_between(g_new_mesh_attributes, g_mesh_attributes)
 
-# Copies parent objects' attributes into child objects' attributes                
-def followParent(self):
-    cmds.text("output", edit=True, label="Select the parent object and click the 'record INITIAL attributes' button")
-    global diff_transform_attributes, diff_mesh_attributes
-    childObjs = cmds.ls(selection=True, dag=True)
-    callBacksForRightType(setChildAttributes, childObjs, diff_transform_attributes, diff_mesh_attributes)
+"""
+change the value of child objects' attributes with
+the value of diff_transform_attributes and g_diff_mesh_attributes.
+"""              
+def change_child_objects_attributes(self):
+    cmds.text("output", e=True, l="Select the parent object and click the 'record INITIAL attributes' button")
+    global g_diff_transform_attributes, g_diff_mesh_attributes
+    child_objects = cmds.ls(sl=True, dag=True)
+    call_back_for_right_type(set_child_objects_attributes, child_objects, g_diff_transform_attributes, g_diff_mesh_attributes)
 
-# find the difference of x and y dictionaries. if there is a difference, keep value of x.
+"""
+find the differences of x and y dictionaries. if there is a difference, keep the value of the item in x.
+"""
 def get_differences_between(x, y):
     return {k: x[k] for k in x if k in y and x[k] != y[k]}
 
-# Populates the attributes of parent objects
-def populateObj(objName, objAttributesReference):
-    print "---------------------------------------------- populateObj"
-    errorAttributes = []
-    allAttributes = cmds.listAttr(objName)
-    for attribute in allAttributes:
-        fullAttributeName = objName + '.' + attribute
+"""
+get the attributes of an object
+"""
+def get_attributes(object_name, object_attributes_ref):
+    error_attributes = []
+    all_attributes = cmds.listAttr(object_name)
+    for attribute in all_attributes:
+        full_attribute_name = object_name + '.' + attribute
         value = None
         try:
-            value = cmds.getAttr(fullAttributeName, sl=True)
+            value = cmds.getAttr(full_attribute_name, sl=True)
         except:
-            errorAttributes.append(fullAttributeName)
-        objAttributesReference[attribute] = value
-    print "For ", objName, ", values cannot get from these attributes: ", errorAttributes
+            error_attributes.append(full_attribute_name)
+        object_attributes_ref[attribute] = value
+    print "For ", object_name, ", values cannot get from these attributes: ", error_attributes
 
-# modifies the correct attributes' type depending on the objects' type
-def callBacksForRightType(callBackFunction, objs, transformAttributes, meshAttributes):
-    for obj in objs: 
-        if cmds.objectType(obj) == "transform":
-            callBackFunction(obj, transformAttributes)
-        elif cmds.objectType(obj) == "mesh":
-            callBackFunction(obj, meshAttributes)
+"""
+Pass the correct pair of object_name and attributes_ref as arguments to the call back function
+"""
+def call_back_for_right_type(call_back_function, object_names, transform_attributes_ref, mesh_attributes_ref):
+    for object_name in object_names: 
+        if cmds.objectType(object_name) == "transform":
+            call_back_function(object_name, transform_attributes_ref)
+        elif cmds.objectType(object_name) == "mesh":
+            call_back_function(object_name, mesh_attributes_ref)
         else:
-            print "Error: ", cmds.objectType(obj), " is unsupported."
+            print "Error: ", cmds.objectType(object_name), " is not supported."
 
-# copies the parents' attributes into the childs' attributes
-def setChildAttributes(objName, objAttributesReference):
-    print "---------------------------------------------- setChildAttributes"
-    errorAttributes = []
-    for attribute in objAttributesReference:
-        fullAttributeName = objName + '.' + attribute
+"""
+set the value of child objects' attributes using
+the value of diff_transform_attributes and g_diff_mesh_attributes.
+"""
+def set_child_objects_attributes(object_name, object_attributes_ref):
+    error_attributes = []
+    for attribute in object_attributes_ref:
+        full_attribute_name = object_name + '.' + attribute
         if attribute not in IGNORED_ATTRIBUTE:
             try:
-                print 'attempting to set ', fullAttributeName, ' to ', objAttributesReference[attribute] 
-                cmds.setAttr(fullAttributeName, objAttributesReference[attribute])
+                cmds.setAttr(full_attribute_name, object_attributes_ref[attribute])
             except:
-                errorAttributes.append(fullAttributeName)
-    print "For ", objName, ", values cannot get from these attributes: ", errorAttributes
-
-
+                error_attributes.append(full_attribute_name)
+    print "For ", object_name, ", values cannot get from these attributes: ", error_attributes
